63c77ba1429a1a7b847c40c5f58bcee91308dcbd
weelittle <cccoolkid@yahoo.com>
2022-12-14 00:26:07 -0500

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs = require('node:fs')
+const fs = require('fs')
 const path = require('node:path')
 
 const { Client, GatewayIntentBits, Partials, Events, Collection } = require('discord.js');
